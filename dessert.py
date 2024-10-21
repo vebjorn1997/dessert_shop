2f5faa6f66adfe6c3ce955b25aa88eb8a0ff63ef
vebjorn1997 <11070506@uvu.edu>
2024-10-21 16:53:38 -0600

    classes implemented

---
+++ dessert.py
@@ -0,0 +1,28 @@
+class DessertItem:
+    def __init__(self, name: str):
+        self.name = name
+
+class Candy(DessertItem):
+    def __init__(self, name: str, candy_weight: float, price_per_pound: float):
+        super().__init__(name)
+        self.candy_weight = candy_weight
+        self.price_per_pound = price_per_pound
+        print(self.name)
+    
+class Cookie(DessertItem):
+    def __init__(self, name: str, cookie_quantity: int, price_per_dozen: float):
+        super().__init__(name)
+        self.cookie_quantity = cookie_quantity
+        self.price_per_dozen = price_per_dozen
+
+class IceCream(DessertItem):
+    def __init__(self, name: str, scoop_count: int, price_per_scoop: float):
+        super().__init__(name)
+        self.scoop_count = scoop_count
+        self.price_per_scoop = price_per_scoop
+
+class Sundae(IceCream):
+    def __init__(self, name: str, scoop_count: int, price_per_scoop: float, topping_name: str, topping_price: float):
+        super().__init__(name, scoop_count, price_per_scoop)
+        self.topping_name = topping_name
+        self.topping_price = topping_price
