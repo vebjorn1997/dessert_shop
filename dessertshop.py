efec6edc5d083c0d38b0d1886d7b25a1b1e1764f
vebjorn1997 <11070506@uvu.edu>
2024-10-21 17:31:47 -0600

    r

---
+++ dessertshop.py
@@ -0,0 +1,35 @@
+from dessert import DessertItem, Candy, Cookie, IceCream, Sundae
+
+
+class Order:
+    def __init__(self):
+        self.order: list[DessertItem] = []
+
+    def add(self, item: DessertItem):
+        self.order.append(item)
+
+    def __len__(self):
+        return len(self.order)
+
+    def __iter__(self):
+        return iter(self.order)
+
+    def __next__(self):
+        return next(self.order)
+
+
+def main():
+    order = Order()
+    order.add(Candy("Candy Corn", 1.5, 0.25))
+    order.add(Candy("Gummy Bears", 0.25, 0.35))
+    order.add(Cookie("Chocolate Chip", 6, 3.99))
+    order.add(IceCream("Pistachio", 2, 0.79))
+    order.add(Sundae("Vanilla", 3, 0.69, "Hot Fudge", 1.29))
+    order.add(Cookie("Oatmeal Raisin", 2, 3.45))
+    for item in order:
+        print(item)
+    print(len(order))
+
+
+if __name__ == "__main__":
+    main()
--- test_dessert.py
+++ test_dessert.py
@@ -1,6 +1,6 @@
-import pytest
-
 from dessert import Candy, Cookie, IceCream, Sundae
+from dessertshop import Order
+
 
 def test_candy():
     candy = Candy("Sjokolade", 5.23, 1.991)
@@ -8,17 +8,20 @@ def test_candy():
     assert candy.candy_weight == 5.23
     assert candy.price_per_pound == 1.991
 
+
 def test_cookie():
     cookie = Cookie("Sjokoladekake", 12, 3.99)
     assert cookie.name == "Sjokoladekake"
     assert cookie.cookie_quantity == 12
     assert cookie.price_per_dozen == 3.99
 
+
 def test_icecream():
     icecream = IceCream("Sjokoladeis", 3, 1.99)
     assert icecream.name == "Sjokoladeis"
     assert icecream.scoop_count == 3
     assert icecream.price_per_scoop == 1.99
+
 
 def test_sundae():
     sundae = Sundae("Sjokoladeiskrem", 2, 1.98, "Oboy", 0.75)
@@ -27,3 +30,11 @@ def test_sundae():
     assert sundae.price_per_scoop == 1.98
     assert sundae.topping_name == "Oboy"
     assert sundae.topping_price == 0.75
+
+
+def test_order():
+    order = Order()
+    order.add(Candy("Candy Corn", 1.5, 0.25))
+    assert len(order) == 1
+    for item in order:
+        assert item.name == "Candy Corn"
